a8d6314b46f6d7e63b60a15014f9fb797a6e9fc5
Beyonce Knowles <beyonce@thinkful.com>
2020-08-11 03:14:37 -0700

    fixed chech/uncheck toggle

--- index.js
+++ index.js
@@ -26,10 +26,15 @@ function shoppingChecklist() {
     });
     // check item on shopping list
     // uncheck item from shopping list
-    $(".shopping-list").on("click", ".shopping-item-delete", $(this).closest("li").remove());
-
+    $(".shopping-list").on("click", ".shopping-item-delete", function(event) {
+        $(this).closest("li").remove()
+    });
+     
+        
     // remove item from shopping list
-    $(".shopping-list").on("click", ".shopping-item-toggle", $(this).closest("li").find(".shopping-item").toggleClass);
+    $(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked")
+    });
 
 
 }
